9f138c9f56c7b1d6afe92c12e0fe74d15fd2e2b4
Haoran Yi <haoran.yi@gmail.com>
2022-08-22 09:50:01 -0500

    up

--- cloud_env.bat
+++ cloud_env.bat
@@ -1,7 +1,7 @@
 ECHO OFF
 CLS
 SET PATH=d:\Google\Cloud SDK\google-cloud-sdk\bin;%PATH%;
-cd d:\Google\Cloud SDK
+:: cd d:\Google\Cloud SDK
 ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
 ECHO ---
 ECHO ON
